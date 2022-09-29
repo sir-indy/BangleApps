96e862720c8e8699872057b154ccf777620ab81f
sir-indy <53864146+sir-indy@users.noreply.github.com>
2022-09-29 15:50:55 +0100

    Fix name of vibration pattern setting

--- apps/messages_notify/lib.js
+++ apps/messages_notify/lib.js
@@ -1,20 +1,20 @@
 exports.pushMessage = function(event) {
   var settings = require('Storage').readJSON("messnot.settings.json", true) || {};
-  if (settings.pattern === undefined) { settings.pattern = ":"; } // pattern may be "", so we can't use || ":" here
+  if (settings.vibrate === undefined) { settings.vibrate = ":"; } // pattern may be "", so we can't use || ":" here
   settings.quiet = ((require('Storage').readJSON('setting.json',1)||{}).quiet);
   if (event.id == 'music') { 
     Bangle.emit('music', event); 
-    return
+    return;
   } 
   if (event.id == 'call') {
     Bangle.emit('call', event);
-    return
+    return;
   }
   // it's a message
   if (event.t == 'remove') {
     require('notify').hide(event);
   } else { // add or modify
     require('notify').show(event);
-    if (!settings.quiet) require("buzz").pattern(settings.pattern);
+    if (!settings.quiet) require("buzz").pattern(settings.vibrate);
   }
-}
+};
