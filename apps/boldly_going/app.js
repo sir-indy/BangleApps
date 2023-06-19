9c1b5082e8ecec43132d1ff8bc01080de0e02e78
sir-indy <53864146+sir-indy@users.noreply.github.com>
2023-06-19 12:16:41 +0100

    Add files via upload

---
+++ apps/boldly_going/app.js
@@ -0,0 +1,171 @@
+{ // must be inside our own scope here so that when we are unloaded everything disappears
+  // we also define functions using 'let fn = function() {..}' for the same reason. function decls are global
+
+  Graphics.prototype.setFontAntonioLarge = function() {
+  // Actual height 50 (50 - 1)
+    return this.setFontCustom(
+      atob('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD8AAAAAAAA/AAAAAAAAPwAAAAAAAD8AAAAAAAA/AAAAAAAAPwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAcAAAAAAAB/AAAAAAAH/wAAAAAAf/8AAAAAB///AAAAAH///wAAAAf///4AAAB////gAAAH///+AAAAf///wAAAB////AAAAH///8AAAAH///wAAAAB///AAAAAAf/8AAAAAAH/wAAAAAAB/AAAAAAAAcAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//////wAD///////AB///////4A////////AP///////wH///////+B////////gfgAAAAAH4H4AAAAAB+B+AAAAAAfgfwAAAAAP4H///////+B////////AP///////wD///////8Af//////+AB//////+AAB/////4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH4AAAAAAAB+AAAAAAAAfAAAAAAAAHwAAAAAAAD8AAAAAAAB+AAAAAAAAfgAAAAAAAP///////AP///////wH///////8B////////Af///////wH///////8A////////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/+AAAP8AH//gAAf/AH//4AAf/wB//+AAf/8A///gAf//AP//4Af//wH//+Af//8B+AAAf/8fAfgAAP/4HwH4AAP/4B8B+AAf/4AfAf////4AHwH////8AB8A////8AAfAP///8AAHwB///8AAB8AH//8AAAfAAP/4AAADwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+AAAfwAAD/wAAH/wAD/8AAB//AB//AAAf/4A//wAAH//AP/8AAB//wH//AAAf/+B/gAPgAA/gfgAD4AAH4H4AA+AAB+B+AAPwAAfgfgAH+AAH4H/AP/8AP+B////////AP///////wD///////4Af//8///+AD//+H//+AAP/+Af/+AAAP4AAf4AAAAAAAAAAAAAAAAAAAAAAAAAAB4AAAAAAAD+AAAAAAAP/gAAAAAAf/4AAAAAA//+AAAAAB///gAAAAH///4AAAAP//4+AAAAf//gPgAAA//+AD4AAD//8AA+AAD//wAAPgAB////////Af///////wH///////8B////////Af///////wH///////8AAAAAAD4AAAAAAAA+AAAAAAAAPgAAAAAAAD4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/wAH///8AP/gB////AD/8Af///wA//gH///8AP/8B////gD//Af///wA//4H4AfgAAD+B+APwAAAfgfgD4AAAH4H4A/AAAB+B+AP/////gfgD/////4H4A/////8B+AH/////AfgA/////gH4AH////wAAAAP///gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//////gAB//////+AB///////wAf//////+AP///////wD///////8B////////gfgAPgAAP4H4AD4AAB+B+AA+AAAfgfgAPgAAH4H+AD8AAD+B//w/////gP/8P////wD//B////8Af/wf///+AD/8D////AAH/AP///AAAAAAP/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAfgAAAAAAAH4AAAAAAAB+AAAAAAAAfgAAAAAPwH4AAAAD/8B+AAAAf//AfgAAD///wH4AAH///8B+AAf////AfgA/////wH4B/////4B+D////gAAfn///4AAAH////gAAAB///8AAAAAf//4AAAAAH//gAAAAAB//AAAAAAAf+AAAAAAAH8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAf4AD//AAB//4D//+AA///j///wAf//9///+AP///////wD///////8B////////gfwAf/AAP4H4AA/AAB+B8AAPgAAfgfAAD4AAH4H4AB/AAB+B/wH/////gf///////wD///////8A////////AH///f///gA///D///wAD//AH//wAAAAAAB+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB//wAAAAAH///wD/gAD////A//AB////4P/4A////+D//AP////w//wH////8P/+B/AAA/AB/gfgAAHwAH4H4AAB8AB+B+AAAfAAfgfwAAHwAH4H///////+A////////AP///////wB///////4Af//////8AB//////+AAH/////8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/AAf4AAAA/wAH+AAAAP8AB/gAAAD/AAf4AAAA/wAH+AAAAP8AB/gAAAD/AAf4AAAAfgAD8AAAAAAAAAAAAAAAAAAAAA='),
+      46,
+      atob("DxQXFxcXFxcXFxcXDQ=="),
+      56|65536
+    );
+  };
+  Graphics.prototype.setFontAntonioSmall = function() {
+  // Actual height 20 (19 - 0)
+    return this.setFontCustom(
+      atob("AAAAAAAAAAAAAAAAAAAA//mP/5gAAAAAAAAAAAAA/gAMAAAAAA/gAPAAAEIIBP+H/8D+IYBP+H/8D+IABCAAwIAfnwP8+PHh448eP3+B4fAAAAAAAH/AD/4AwGAMBgD/4Af8GAAPgAPgAfgAfAAfAA+AAOP/AH/4BgGAYBgH/4A/8AAAAAAAAAQAA/B+f4/+GMPhjv/4/h8Dg/gAcYwAAPwADgAAAAAAAAB//8///sAAaAACAAAMAAb//+f//AAAAAAAbAAGwAA4AA/wADgABsAAbAAAAAAAgAAMAAPwAD8AAMAADAAAAAAAAAAHAAB/AAOAAAAAAAAMAADAAAwAAMAACAAAAAAAAAABgAAYAAAAAAAAA4AD+AP+A/4A/gAOAAAAAAAAAH//j//8wADMAAz//8f/+AAAAAAAMAADAABgAA//+P//gAAAAAAAAAAAAAfgfP4fzAfswfDP/gx/gMAAAHgPj4D8wMDMHAz//8f3+AAEAAAAADwAH8APzA/AwP//j//4AAwAAAD/Hw/x+MwBjOAYz/+Mf/AAAAAAAH//j//8wYDMGAz9/8fP+AAcDAAAwAAMAfjB/4z/wP+AD4AAwAAAAOB/f4///MHAzBwM///H9/gAAAAAAH/Pj/78wGDMBgz//8f/+AAAAAAADhwA4cAAAAAAAAAAAAAADh/A4fgAAAAOAAHwABsAA7gAccAGDAAAAANgADYAA2AANgADYAA2AAAAAAAABgwAccADuAAbAAHwAA4AAAAHwAD8c4/POMHAD/wAfwAAAAAAAAD/wD//B4B4Y/HMf8zMBMyATMwczP+M4BzHwcgf+AA+AAAAAAD4A/+P/8D+DA/4wH/+AB/4AAeAAAAAAA//+P//jBgYwYGP//j//4PH4AAAAAAAf/+P//zgAcwADP4fz+P4Ph8AAAAAAA//+P//jAAYwAGPADj//4P/4AAAAAAA//+P//jBgYwYGMGBgAAAAAAP//j//4wYAMGADBgAAAAAAAA//w///PAHzAQM4MHP7/x+/8AAAAAAD//4//+AGAABgAAYAP//j//4AAAAAAAAAA//+P//gAAAAAAAAAAAHwAB+AABgAAY//+P//AAAAAAAAAAD//4//+APgAf+Afj8PgPjAAYAAAAAAD//4//+AABgAAYAAGAAAAAAA//+P//j/gAD/wAB/gAP4B/4P/AD//4//+AAAAAAAAAAP//j//4P4AAfwAA/g//+P//gAAAAAAAAAA//g//+PAHjAAY4AOP//h//wAAAAAAD//4//+MDADAwA4cAP/AB/gAAAAAAAA//g//+PAHjAAc4APv//5//yAAAAAAD//4//+MGADBgA48AP//h+f4AAAAAAB+Pw/z+MOBjBwY/P+Hx/AAHgwAAMAAD//4//+MAADAAAAAAP//D//4AAOAABgAA4//+P//AAAAwAAP8AD//AA/+AAfgP/4//gPwAAAAA+AAP/4Af/4AD+A//j/wA/wAD/+AA/4B/+P/+D+AAAAAMADj8P4P/4A/4B//w+A+MABgAAA4AAPwAB/gAB/+A//j/gA+AAMAAAAAYwB+MH/jf+Y/8GPwBjAAAAAAP//7//+wABsAAYAAAAAAPAAD/gAH/gAD/gAD4AACAAADAAGwABv//7//+AAAA=="),
+      32,
+      atob("BQUHCAgVCQQFBQkHBQcFBwgICAgICAgICAgFBQcHBwgPCQkJCQcHCQoFCQkHDQoJCQkJCAYJCQ0ICAcGBwY="),
+      20
+    );
+  };
+  
+  const CORNER1 = require("heatshrink").decompress(atob("mESwIEBv//AAUBAgf/g4FEh4FEj4FEn4FJFAn8Aon4gAQDAovwAtQA=="));
+  const CORNER2 = require("heatshrink").decompress(atob("mESwMA/4AC+AFu/EAv4FI/kAn4SDAov/j4FEh4FEg4FEgIFEFQn/"));
+  const FONT_SIZE_SMALL = 20;
+  const FONT_SIZE_LARGE = 50;
+  let R = Bangle.appRect;
+  let F = {w:36, h:6, ch:18, cw:48, gap:2, top_h:38}; //ch = corner image height, cw = corner image width
+  F.info_h = (R.h - F.top_h - F.ch * 2 - F.gap*5)/2;
+  let drawFrameTimeout;
+  let drawTimeTimeout;
+
+  const COLOUR = {BLACK: '#000', YELLOW: '#FF8', PURPLE: '#C8C', BLUE: '#8CF',
+                  WHITE: '#FFF', GREEN: '#CFC', ORANGE: '#F84'};
+  let C = {bg: COLOUR.BLACK, text: COLOUR.YELLOW, h1: COLOUR.ORANGE, bar: COLOUR.YELLOW, 
+           frame1: COLOUR.PURPLE, frame2: COLOUR.BLUE};
+
+  let drawFrame = function() {
+    // queue next draw
+    if (drawFrameTimeout) clearTimeout(drawFrameTimeout);
+    drawTimeout = setTimeout(function() {
+      drawFrameTimeout = undefined;
+      drawTime();
+    }, 8.64e7 - (Date.now() % 8.64e7));
+    g.reset().setBgColor(C.bg).clear();
+
+    //DATE FRAME
+    g.setColor(C.frame1).drawImage(CORNER1, 0, 0);
+    g.drawImage(CORNER2, 0, F.top_h - F.ch);
+    g.setColor(C.frame1).fillRect(0, F.ch, F.w - 1, F.top_h - F.ch);
+    g.setColor(C.frame1).fillRect(F.ch, 0, R.w, F.h-1);
+    g.setColor(C.frame1).fillRect(F.cw + F.gap, F.top_h-F.h, R.w, F.top_h-1);
+
+    //TIME FRAME
+    g.setColor(C.frame2).drawImage(CORNER1, 0, F.top_h + F.gap*2);
+    g.setColor(C.frame2).drawImage(CORNER2, 0, R.h - F.ch);
+    g.setColor(C.frame2).fillRect(F.ch, F.top_h + F.gap*2, 100-1, F.top_h + F.gap*2 + F.h - 1);
+    g.setColor(C.frame2).fillRect(F.ch, R.h - F.h, R.w, R.h);
+    g.setColor(C.frame1).fillRect(100 + F.gap, F.top_h + F.gap*2, R.w, F.top_h + F.gap*2 + F.h - 1);
+
+    //FRAME TEXT
+    g.setFont("4x6").setColor(C.text).setFontAlign(1, -1);
+    g.clearRect(R.w - F.h - g.stringWidth('STARDATE'), 0, R.w - F.h, F.h);
+    g.drawString('STARDATE',R.w - F.h + 1,1);
+    g.clearRect(R.w - F.h - g.stringWidth('BANGLE LCARS'), R.h - F.h, R.w - F.h, R.h);
+    g.drawString('BANGLE LCARS',R.w - F.h + 1,R.h - F.h);
+
+    //DAY OF WEEK, DATE and TIME
+    let t = new Date();
+    let dow_string = require("locale").dow(t, 1).toUpperCase();
+    let date_string = require("locale").date(t, 1);//.split('/').join('.');
+    g.setFont('AntonioSmall').setColor(C.bg).setFontAlign(0, 0).drawString(dow_string, F.w/2, F.top_h/2);
+    g.setFont('AntonioSmall').setColor(C.text).setFontAlign(0, 0).drawString(date_string, (R.w + F.w)/2, F.top_h/2);
+  };
+
+  let drawTime = function() {
+    // queue next draw
+    if (drawTimeTimeout) clearTimeout(drawTimeTimeout);
+    drawTimeout = setTimeout(function() {
+      drawTimeTimeout = undefined;
+      drawTime();
+    }, 60000 - (Date.now() % 60000));
+    g.reset().setBgColor(C.bg).setColor(C.text);
+    g.clearRect(F.w, F.top_h + F.ch, R.w, F.top_h + F.ch + FONT_SIZE_LARGE);
+    let time_string = require("locale").time(new Date(), 1);//.replace(':','');
+    g.setFont('AntonioLarge').setFontAlign(0, -1).drawString(time_string, (R.w + F.w)/2, F.top_h + F.ch);
+  };
+
+  // clock info menus (scroll up/down for info)
+  let clockInfoDrawSide = (itm, info, options) => {
+    // set a cliprect to stop us drawing outside our box
+    g.reset().setClipRect(options.x, options.y, options.x+options.w-1, options.y+options.h-1);
+    g.setColor(C.bg).setBgColor(C.frame2);
+    if (options.focus) g.setBgColor(C.h1); // show if focused
+    g.clearRect(options.x, options.y, options.x+options.w-2, options.y+options.h-1);
+    // we're drawing center-aligned here
+    var midx = options.x+options.w/2;
+    if (info.img) g.drawImage(info.img, midx-12,options.y+F.gap); // draw the image
+    g.setFont('AntonioSmall').setFontAlign(0,1).drawString(info.text, midx,options.y+options.h); // draw the text
+    g.setClipRect(0,0,g.getWidth()-1, g.getHeight()-1);
+  };
+
+  let clockInfoDrawMain = (itm, info, options) => {
+    // set a cliprect to stop us drawing outside our box
+    g.reset().setClipRect(options.x, options.y, options.x+options.w-1, options.y+options.h-1);
+    g.setColor(C.h1).setBgColor(C.bg);
+    g.clearRect(options.x, options.y, options.x+options.w-1, options.y+options.h-1);
+    if (options.focus) { // show if focused
+      g.fillRect({x: options.x, y: options.y, w: options.w - 1, h: options.h - 1, r: F.h});
+      g.setColor(C.bg).fillRect({
+        x: options.x + F.gap, y: options.y + F.gap * 2,
+        w: options.w - F.gap * 2 - 1, h: options.h  - F.gap * 4 - 1, r: F.gap*2
+      });
+      g.fillRect({x: options.x + F.gap*2, y: options.y, w: options.w - F.gap*4 - 1, h: options.h - 1});
+    }
+    let bar_offset = 0; // offset from centre if progress bar shown
+    if (itm.hasRange) { // draw progress bar
+      bar_offset = F.h/2;
+      let d = itm.get();
+      let bar = {x: F.h*2, y: options.h - F.h - F.gap, w: options.w - F.h*4, h: F.h};
+      g.setColor('#444').fillRect({x: options.x + bar.x, y: options.y + bar.y, w: bar.w, h: bar.h});
+      g.setColor(C.bar).fillRect({
+        x: options.x + bar.x, y: options.y + bar.y, w: bar.w * (d.v/(d.max-d.min)), h: bar.h
+      });
+    }
+    g.setColor(C.text);
+    if (info.img) g.drawImage(info.img, options.x + F.h, options.y+options.h/2 - 12 - bar_offset); // draw the image
+    g.setFont('AntonioSmall').setFontAlign(-1,0);
+    g.drawString(info.text, options.x + F.h*2 + 24, options.y+options.h/2 - bar_offset); // draw the text
+    g.setClipRect(0,0,g.getWidth()-1, g.getHeight()-1); // release the clipRect
+  };
+
+  let clockInfoItems = require("clock_info").load();
+  let clockInfoMenu = require("clock_info").addInteractive(clockInfoItems, {  // right top
+    x:0, y:F.top_h + F.ch + F.gap*3, w: F.w + 1, h: F.info_h, draw : clockInfoDrawSide
+  });
+  let clockInfoMenu2 = require("clock_info").addInteractive(clockInfoItems, { // right bottom
+    x:0, y:F.top_h + F.ch + F.gap*4 + F.info_h, w: F.w + 1, h: F.info_h, draw : clockInfoDrawSide
+  });
+  let clockInfoMenu3 = require("clock_info").addInteractive(clockInfoItems, { // main screen
+    x:F.w + F.h, y:R.h - F.h*2 - FONT_SIZE_LARGE, w: R.w - F.w - F.h*2, h: FONT_SIZE_LARGE, draw : clockInfoDrawMain
+  });
+
+  // Show launcher when middle button pressed
+  Bangle.setUI({
+    mode : "clock",
+    remove : function() {
+      // Called to unload all of the clock app
+      if (drawTimeTimeout) clearTimeout(drawTimeTimeout);
+      drawTimeTimeout = undefined;
+      if (drawFrameTimeout) clearTimeout(drawFrameTimeout);
+      drawFrameTimeout = undefined;
+      // remove info menu
+      clockInfoMenu.remove();
+      delete clockInfoMenu;
+      clockInfoMenu2.remove();
+      delete clockInfoMenu2;
+      clockInfoMenu3.remove();
+      delete clockInfoMenu3;
+      require("widget_utils").show();
+    }
+  });
+
+  // Load widgets
+  Bangle.loadWidgets();
+  require("widget_utils").swipeOn();
+
+  drawFrame();
+  drawTime();
+
+}
